a8c5c089a50a5cc76e27c942c2f58e808e31daac
Lucas Walter <wsacul@gmail.com>
2024-10-13 13:24:44 -0700

    construct a full cube

--- bevy_test/src/bin/mesh_vert_moving.rs
+++ bevy_test/src/bin/mesh_vert_moving.rs
@@ -24,7 +24,7 @@ fn main() {
 #[derive(Component)]
 struct AnimatedJoint;
 
-fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, dp: f32, ht: f32) -> Vec<(Mesh, Color)> {
+fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, ht: f32, dp: f32) -> Vec<(Mesh, Color)> {
     let mut mesh_colors = Vec::new();
 
     {
@@ -47,8 +47,8 @@ fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, dp: f32, ht: f32) -> Vec<(Mesh
     {
         // front
         let pts = vec![
-            [x0 + -wd, y0 + 0.0, z0 + dp],
-            [x0 + wd, y0 + 0.0, z0 + dp],
+            [x0 + -wd, y0 - ht, z0 + dp],
+            [x0 + wd, y0 - ht, z0 + dp],
             [x0 + -wd, y0 + ht, z0 + dp],
             [x0 + wd, y0 + ht, z0 + dp],
         ];
@@ -64,8 +64,8 @@ fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, dp: f32, ht: f32) -> Vec<(Mesh
     {
         // right
         let pts = vec![
-            [x0 + wd, y0 + 0.0, z0 + dp],
-            [x0 + wd, y0 + 0.0, z0 - dp],
+            [x0 + wd, y0 - ht, z0 + dp],
+            [x0 + wd, y0 - ht, z0 - dp],
             [x0 + wd, y0 + ht, z0 + dp],
             [x0 + wd, y0 + ht, z0 - dp],
         ];
@@ -75,14 +75,14 @@ fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, dp: f32, ht: f32) -> Vec<(Mesh
             .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[1.0, 0.0, 0.0]; num_pts])
             .with_indices(Some(Indices::U16(vec![0, 1, 2, 1, 3, 2])));
 
-        mesh_colors.push((mesh, Color::rgb(0.2, 0.75, 0.0)));
+        mesh_colors.push((mesh, Color::rgb(0.8, 0.15, 0.0)));
     }
 
     {
         // back
         let pts = vec![
-            [x0 + wd, y0 + 0.0, z0 - dp],
-            [x0 + -wd, y0 + 0.0, z0 - dp],
+            [x0 + wd, y0 - ht, z0 - dp],
+            [x0 + -wd, y0 - ht, z0 - dp],
             [x0 + wd, y0 + ht, z0 - dp],
             [x0 + -wd, y0 + ht, z0 - dp],
         ];
@@ -98,8 +98,8 @@ fn setup_rect(x0: f32, y0: f32, z0: f32, wd: f32, dp: f32, ht: f32) -> Vec<(Mesh
     {
         // left
         let pts = vec![
-            [x0 + -wd, y0 + 0.0, z0 - dp],
-            [x0 + -wd, y0 + 0.0, z0 + dp],
+            [x0 + -wd, y0 - ht, z0 - dp],
+            [x0 + -wd, y0 - ht, z0 + dp],
             [x0 + -wd, y0 + ht, z0 - dp],
             [x0 + -wd, y0 + ht, z0 + dp],
         ];
@@ -126,7 +126,8 @@ fn setup(
     // Create a camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-2.0, 5.0, 5.0)
+                .looking_at(Vec3::new(0.0, 3.0, 0.0), Vec3::Y),
             ..default()
         },
         bevy_test::CameraController::default(),
@@ -136,7 +137,7 @@ fn setup(
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 7000.,
-            range: 100.,
+            range: 50.,
             shadows_enabled: true,
             ..default()
         },
@@ -151,14 +152,31 @@ fn setup(
         ..default()
     });
 
-    let mesh_colors = setup_rect(0.0, 2.0, 0.0, 0.1, 0.15, 1.0);
-    for (mesh, color) in mesh_colors {
-        let mesh = meshes.add(mesh);
-        commands.spawn(PbrBundle {
-            mesh,
-            material: materials.add(color.into()),
-            ..default()
-        });
+    let mut mc = Vec::new();
+    mc.push(setup_rect(-1.0, 3.0, -1.0, 0.05, 1.0, 0.1));
+    mc.push(setup_rect(-1.0, 3.0, 1.0, 0.05, 1.0, 0.1));
+    mc.push(setup_rect(1.0, 3.0, -1.0, 0.05, 1.0, 0.1));
+    mc.push(setup_rect(1.0, 3.0, 1.0, 0.05, 1.0, 0.1));
+
+    mc.push(setup_rect(0.0, 2.0, -1.0, 1.0, 0.05, 0.12));
+    mc.push(setup_rect(0.0, 2.0, 1.0, 1.0, 0.05, 0.12));
+    mc.push(setup_rect(0.0, 4.0, -1.0, 1.0, 0.05, 0.12));
+    mc.push(setup_rect(0.0, 4.0, 1.0, 1.0, 0.05, 0.12));
+
+    mc.push(setup_rect(-1.0, 2.0, 0.0, 0.05, 0.05, 1.0));
+    mc.push(setup_rect(1.0, 2.0, 0.0, 0.05, 0.05, 1.0));
+    mc.push(setup_rect(-1.0, 4.0, 0.0, 0.05, 0.05, 1.0));
+    mc.push(setup_rect(1.0, 4.0, 0.0, 0.05, 0.05, 1.0));
+
+    for mesh_colors in mc {
+        for (mesh, color) in mesh_colors {
+            let mesh = meshes.add(mesh);
+            commands.spawn(PbrBundle {
+                mesh,
+                material: materials.add(color.into()),
+                ..default()
+            });
+        }
     }
 
     // Create inverse bindpose matrices for a skeleton consists of 2 joints
@@ -282,6 +300,7 @@ fn setup(
 /// Animate the joint marked with [`AnimatedJoint`] component.
 fn joint_animation(time: Res<Time>, mut query: Query<&mut Transform, With<AnimatedJoint>>) {
     for mut transform in &mut query {
-        transform.rotation = Quat::from_rotation_z(FRAC_PI_2 * time.elapsed_seconds().sin());
+        transform.translation.x = time.elapsed_seconds().sin();
+        // transform.rotation = Quat::from_rotation_z(FRAC_PI_2 * time.elapsed_seconds().sin());
     }
 }
