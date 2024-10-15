5f6861f0cdc5ace3cde0490fea31e0a73d945afc
Lucas Walter <wsacul@gmail.com>
2024-10-14 18:45:50 -0700

    now have full tesseract, next need to animate it

--- bevy_test/src/bin/mesh_vert_moving.rs
+++ bevy_test/src/bin/mesh_vert_moving.rs
@@ -16,20 +16,17 @@ struct Beams4D {
 }
 
 impl Beams4D {
-    fn build_points(&mut self) {
-        // each of these will be a plane made of four points
-        let x0 = 0.0;
-        let xsz = 0.2;
-
-        let y0 = 4.0;
-        let ysz = 0.3;
-
-        let z0 = 0.0;
-        let zsz = 0.4;
-
-        let w0 = 1.0;
-        let wsz = 1.0;
-
+    fn build_points(
+        &mut self,
+        x0: f64,
+        xsz: f64,
+        y0: f64,
+        ysz: f64,
+        z0: f64,
+        zsz: f64,
+        w0: f64,
+        wsz: f64,
+    ) {
         let xs = [x0 - xsz, x0 + xsz];
         let ys = [y0 - ysz, y0 + ysz];
         let zs = [z0 - zsz, z0 + zsz];
@@ -147,6 +144,24 @@ impl Beams4D {
             Vector4::new(xs[0], ys[1], zs[0], ws[0]),
             Vector4::new(xs[0], ys[1], zs[0], ws[1]),
         ];
+        let yw1 = vec![
+            Vector4::new(xs[1], ys[0], zs[0], ws[0]),
+            Vector4::new(xs[1], ys[0], zs[0], ws[1]),
+            Vector4::new(xs[1], ys[1], zs[0], ws[0]),
+            Vector4::new(xs[1], ys[1], zs[0], ws[1]),
+        ];
+        let yw2 = vec![
+            Vector4::new(xs[0], ys[0], zs[1], ws[0]),
+            Vector4::new(xs[0], ys[0], zs[1], ws[1]),
+            Vector4::new(xs[0], ys[1], zs[1], ws[0]),
+            Vector4::new(xs[0], ys[1], zs[1], ws[1]),
+        ];
+        let yw3 = vec![
+            Vector4::new(xs[1], ys[0], zs[1], ws[0]),
+            Vector4::new(xs[1], ys[0], zs[1], ws[1]),
+            Vector4::new(xs[1], ys[1], zs[1], ws[0]),
+            Vector4::new(xs[1], ys[1], zs[1], ws[1]),
+        ];
 
         let zw0 = vec![
             Vector4::new(xs[0], ys[0], zs[0], ws[0]),
@@ -154,12 +169,30 @@ impl Beams4D {
             Vector4::new(xs[0], ys[0], zs[1], ws[0]),
             Vector4::new(xs[0], ys[0], zs[1], ws[1]),
         ];
-
-        // self.points_4d = vec![xy0, xz0, xw0, yz0, yw0, zw0];
-        self.points_4d = vec![
-            xy0, xy1, xy2, xy3, xz0, xz1, xz2, xz3, yz0, yz1, yz2, yz3, xw0, xw1, xw2, xw3, yw0,
-            zw0,
+        let zw1 = vec![
+            Vector4::new(xs[1], ys[0], zs[0], ws[0]),
+            Vector4::new(xs[1], ys[0], zs[0], ws[1]),
+            Vector4::new(xs[1], ys[0], zs[1], ws[0]),
+            Vector4::new(xs[1], ys[0], zs[1], ws[1]),
         ];
+        let zw2 = vec![
+            Vector4::new(xs[0], ys[1], zs[0], ws[0]),
+            Vector4::new(xs[0], ys[1], zs[0], ws[1]),
+            Vector4::new(xs[0], ys[1], zs[1], ws[0]),
+            Vector4::new(xs[0], ys[1], zs[1], ws[1]),
+        ];
+        let zw3 = vec![
+            Vector4::new(xs[1], ys[1], zs[0], ws[0]),
+            Vector4::new(xs[1], ys[1], zs[0], ws[1]),
+            Vector4::new(xs[1], ys[1], zs[1], ws[0]),
+            Vector4::new(xs[1], ys[1], zs[1], ws[1]),
+        ];
+
+        let points_4d = vec![
+            xy0, xy1, xy2, xy3, xz0, xz1, xz2, xz3, yz0, yz1, yz2, yz3, xw0, xw1, xw2, xw3, yw0,
+            yw1, yw2, yw3, zw0, zw1, zw2, zw3,
+        ];
+        self.points_4d.extend(points_4d);
     }
 
     // get points and a normal
@@ -210,7 +243,8 @@ impl Beams4D {
                 .with_indices(Some(Indices::U16(vec![0, 2, 1, 1, 2, 3])));
 
             let indf = ind as f32;
-            let color = Color::rgb(indf * 0.1, 1.0 - indf * 0.05, indf * 0.01);
+            // let color = Color::rgb((indf * 0.1) % 1.0, (1.0 - indf * 0.05) % 1.0, (indf * 0.01) % 1.0);
+            let color = Color::rgb(0.65, 0.6, 0.5);
             println!("{ind} {indf} {color:?}");
             meshes_colors.push((mesh, color));
         }
@@ -256,10 +290,75 @@ impl Beams4D {
 
 fn main() {
     let mut beams_4d = Beams4D {
-        i: 0,
+        _i: 0,
         points_4d: Vec::new(),
     };
-    beams_4d.build_points();
+
+    // build beams of a tesseract
+    for yi in 0..2 {
+        for zi in 0..2 {
+            for wi in 0..2 {
+                let x0 = 0.0; // xi as f64 * 2.0 - 1.0;
+                let xsz = 1.0;
+                let y0 = yi as f64 * 2.0 - 1.0;
+                let ysz = 0.06;
+                let z0 = zi as f64 * 2.0 - 1.0;
+                let zsz = 0.07;
+                let w0 = 1.0 + wi as f64 * 2.0 - 1.0;
+                let wsz = 0.08;
+                beams_4d.build_points(x0, xsz, y0, ysz, z0, zsz, w0, wsz);
+            }
+        }
+    }
+
+    for xi in 0..2 {
+        for zi in 0..2 {
+            for wi in 0..2 {
+                let x0 = xi as f64 * 2.0 - 1.0;
+                let xsz = 0.05;
+                let y0 = 0.0; // yi as f64 * 2.0 - 1.0;
+                let ysz = 1.0;
+                let z0 = zi as f64 * 2.0 - 1.0;
+                let zsz = 0.07;
+                let w0 = 1.0 + wi as f64 * 2.0 - 1.0;
+                let wsz = 0.08;
+                beams_4d.build_points(x0, xsz, y0, ysz, z0, zsz, w0, wsz);
+            }
+        }
+    }
+
+    for xi in 0..2 {
+        for yi in 0..2 {
+            for wi in 0..2 {
+                let x0 = xi as f64 * 2.0 - 1.0;
+                let xsz = 0.05;
+                let y0 = yi as f64 * 2.0 - 1.0;
+                let ysz = 0.06;
+                let z0 = 0.0;
+                let zsz = 1.0;
+                let w0 = 1.0 + wi as f64 * 2.0 - 1.0;
+                let wsz = 0.08;
+                beams_4d.build_points(x0, xsz, y0, ysz, z0, zsz, w0, wsz);
+            }
+        }
+    }
+
+    for xi in 0..2 {
+        for yi in 0..2 {
+            for zi in 0..2 {
+                let x0 = xi as f64 * 2.0 - 1.0;
+                let xsz = 0.05;
+                let y0 = yi as f64 * 2.0 - 1.0;
+                let ysz = 0.06;
+                let z0 = zi as f64 * 2.0 - 1.0;
+                let zsz = 0.07;
+                let w0 = 1.0;
+                let wsz = 1.0;
+                beams_4d.build_points(x0, xsz, y0, ysz, z0, zsz, w0, wsz);
+            }
+        }
+    }
+
     // TODO(lucasw) double clone seems wrong, but it works
     let raw_meshes = beams_4d.get_meshes();
 
@@ -298,8 +397,8 @@ fn setup(
     // Create a camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 5.0, 5.0)
-                .looking_at(Vec3::new(0.0, 3.0, 0.0), Vec3::Y),
+            transform: Transform::from_xyz(-2.0, 0.0, 5.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
             ..default()
         },
         bevy_test::CameraController::default(),
@@ -345,8 +444,9 @@ fn setup(
 
     // ground plane
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(50.).into()),
+        mesh: meshes.add(shape::Plane::from_size(150.).into()),
         material: materials.add(Color::SILVER.into()),
+        transform: Transform::from_xyz(0.0, -2.0, 0.0),
         ..default()
     });
 }
