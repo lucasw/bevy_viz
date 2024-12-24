de630c0443edac7cdc07ac8e3e4eee20a9e198de
Lucas Walter <wsacul@gmail.com>
2024-12-24 08:43:37 -0800

    now a single body for the car chassis

--- vehicle/src/bin/vehicle.rs
+++ vehicle/src/bin/vehicle.rs
@@ -27,59 +27,35 @@ pub fn setup_graphics(mut commands: Commands) {
 }
 
 pub fn setup_physics(mut commands: Commands) {
-    /*
-     * Ground
-     */
-    let ground_size = 200.1;
-    let ground_height = 0.1;
+    // ground
+    {
+        let ground_size = 200.1;
+        let ground_height = 0.1;
 
-    commands.spawn((
-        TransformBundle::from(Transform::from_xyz(0.0, -ground_height, 0.0)),
-        Collider::cuboid(ground_size, ground_height, ground_size),
-    ));
+        commands.spawn((
+                TransformBundle::from(Transform::from_xyz(0.0, -ground_height, 0.0)),
+                Collider::cuboid(ground_size, ground_height, ground_size),
+        ));
+    }
 
-    /*
-     * Create the cubes
-     */
-    let num = 3;
-    let rad = 1.0;
+    // car chassis
+    {
+        // y is vertical
+        let x = 0.0;
+        let y = 20.0;
+        let z = 0.0;
 
-    let shift = rad * 2.0 + rad;
-    let centerx = shift * (num / 2) as f32;
-    let centery = shift / 2.0;
-    let centerz = shift * (num / 2) as f32;
-
-    let mut offset = -(num as f32) * (rad * 2.0 + rad) * 0.5;
-    let mut color = 0;
-    let colors = [
-        Hsla::hsl(220.0, 1.0, 0.3),
-        Hsla::hsl(180.0, 1.0, 0.3),
-        Hsla::hsl(260.0, 1.0, 0.7),
-    ];
-
-    for j in 0usize..20 {
-        for i in 0..num {
-            for k in 0usize..num {
-                let x = i as f32 * shift - centerx + offset;
-                let y = j as f32 * shift + centery + 3.0;
-                let z = k as f32 * shift - centerz + offset;
-                color += 1;
-
-                commands
-                    .spawn(TransformBundle::from(Transform::from_rotation(
-                        Quat::from_rotation_x(0.2),
-                    )))
-                    .with_children(|child| {
-                        child.spawn((
-                            TransformBundle::from(Transform::from_xyz(x, y, z)),
-                            RigidBody::Dynamic,
-                            Collider::cuboid(rad * 2.0, rad, rad * 0.5),
-                            ColliderDebugColor(colors[color % 3]),
-                        ));
-                    });
-            }
-        }
-
-        offset -= 0.05 * rad * (num as f32 - 1.0);
+        commands
+            .spawn(TransformBundle::from(Transform::from_rotation(
+                        Quat::from_rotation_x(0.4) * Quat::from_rotation_z(0.2),
+            )))
+            .with_children(|child| {
+                child.spawn((
+                        TransformBundle::from(Transform::from_xyz(x, y, z)),
+                        RigidBody::Dynamic,
+                        Collider::cuboid(4.0, 0.5, 2.0),
+                        ColliderDebugColor(Hsla::hsl(220.0, 1.0, 0.3)),
+                ));
+            });
     }
 }
